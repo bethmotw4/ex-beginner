b490bded80d554e17b7d0c08aeb8b4ce58a88f8e
森本 義基 <yoshiki.morimoto@rakus.internal>
2020-06-16 17:07:25 +0900

    exam05.htmlを作成

---
+++ src/main/java/com/example/controller/Exam04Controller.java
@@ -0,0 +1,38 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.domain.User;
+import com.example.form.UserForm;
+
+@Controller
+@RequestMapping("/exam04")
+public class Exam04Controller {
+	@ModelAttribute
+	public UserForm setUpForm() {
+		return new UserForm();
+	}
+	
+	@RequestMapping("")
+	public String index(Model model) {
+		return "exam04";
+	}
+	
+	@RequestMapping("/result")
+	public String result(@Validated UserForm userForm, BindingResult result, Model model) {
+		if (result.hasErrors()) {
+			return index(model);
+		}
+		User user = new User();
+		user.setName(userForm.getName());
+		user.setAge(userForm.getAge());
+		user.setComment(userForm.getComment());
+		model.addAttribute("user", user);
+		return "exam04-result";
+	}
+}
